7724b0217fb8eb5e5b3678caf11695d38ccc312e
Zycrix <Doffa98@live.no>
2022-09-01 20:59:59 +0200

    Finishing the page

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const searchBar = document.querySelector(".search");
+const searchBar = document.querySelector(".search-field");
 const searchIcon = document.querySelector(".s-icon");
-const close = document.querySelector(".close");
-const profilePicture = document.querySelector(".profile-picture");
+const profilePicture = document.querySelectorAll(".profile-picture");
 const profileName = document.querySelector(".post-name");
+const headerProfile = document.querySelector(".header-profile");
+const headerLogin = document.querySelector(".header-login");
+const logout = document.querySelector(".logout");
 
 searchIcon.addEventListener("click", (e)=>{
   searchBar.classList.toggle("hidden");
-  searchIcon.classList.toggle("hidden");
-});
-close.addEventListener("click", (e)=>{
-  searchBar.classList.toggle("hidden");
-  searchIcon.classList.toggle("hidden");
 });
 
-profilePicture.addEventListener("click", (e)=>{
-  window.location.href = `${window.origin}/profile.html`
+if(window.location.href === `${window.origin}/index.html`){
+  profileName.addEventListener("click", (e)=>{
+    window.location.href = `${window.origin}/profile.html`
+  });
+}
+profilePicture.forEach((e)=>{
+  e.addEventListener("click", (e)=>{
+    window.location.href = `${window.origin}/profile.html`
+  });
 })
-profileName.addEventListener("click", (e)=>{
-  window.location.href = `${window.origin}/profile.html`
-})
+if(window.sessionStorage.getItem("loggedin") === "true"){
+  headerLogin.classList.add("hidden");
+  headerProfile.classList.remove("hidden");
+}
+
+if(window.location.href === `${window.origin}/login.html`){
+  window.sessionStorage.setItem("loggedin", "true");
+}
+
+
+logout.addEventListener("click", (e)=>{
+  window.sessionStorage.clear();
+  window.location.reload();
+});
